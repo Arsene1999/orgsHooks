bcedeebcf6126743ded26047901cb5dd535fee58
João Victo Dantas <67969013+Arsene1999@users.noreply.github.com>
2024-06-17 16:40:45 -0300

    topo ok

---
+++ src/servicos/carregaDados.js
@@ -0,0 +1,8 @@
+import produtores from "../mocks/produtores";
+import topo from "../mocks/topo";
+
+const carregaProdutores = () => produtores;
+
+const carregaTopo = () => topo;
+
+export {carregaProdutores, carregaTopo};
